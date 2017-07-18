dd6012faa9fdda1e997b7e51b9800b2037ce75c5
namaedevibar <namaiidevibar@gmail.com>
2017-07-18 17:02:51 +0800

    Inflate fragments for the two teams

--- app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
+++ app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
@@ -1,9 +1,11 @@
 package tech.hyperdev.scorekeeper.activities;
 
+import android.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import tech.hyperdev.scorekeeper.R;
+import tech.hyperdev.scorekeeper.fragments.ScoreFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ScoreFragment scoreFragmentA = new ScoreFragment();
+        ScoreFragment scoreFragmentB = new ScoreFragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_1,scoreFragmentA).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_2,scoreFragmentB).commit();
+
     }
 }
